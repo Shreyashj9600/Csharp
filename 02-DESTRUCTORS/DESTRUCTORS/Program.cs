05d7c36e224ef923b78dd3339308bdb2b9f32bd6
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-17 00:39:42 +0530

    Destructors

---
+++ 02-DESTRUCTORS/DESTRUCTORS/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DESTRUCTORS
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age) 
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string getName () 
+        {
+            return this.name;
+        }
+        public int getAge()
+        {
+            return this.age; 
+        }
+        ~Person() 
+        {
+            Console.WriteLine("Destructor is innovk !!..");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person rohit = new Person("rohit", 11);
+            Console.WriteLine(rohit.getName());
+            Console.WriteLine(rohit.getAge());
+           
+            //Console.ReadLine();
+        }
+    }
+}
