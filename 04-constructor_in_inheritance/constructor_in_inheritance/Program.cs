759a4cd77e979215a0c0b32223ae19bf58edca94
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-17 01:38:58 +0530

    CONSTRUCTOR IN INHERTANCE

---
+++ 04-constructor_in_inheritance/constructor_in_inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+
+namespace constructor_in_inheritance
+{
+    class BaseClass 
+    {
+        public BaseClass(string massage ) 
+        {
+            Console.WriteLine("this is base class constructor" + " " + massage);
+        }
+    }
+    class DeriverdClass : BaseClass
+    {
+       public DeriverdClass() : base("this is c sharp")
+        {
+            Console.WriteLine("this is Derived class constructor ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeriverdClass obj = new DeriverdClass();
+
+            Console.ReadLine();
+        }
+    }
+}
