3f43b0bbbc2c1562931e11695a414ec9a89855dc
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-20 23:42:57 +0530

    Method Hiding

---
+++ 10-METHOD HIDING/METHOD HIDING/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace METHOD_HIDING
+{
+    class Parent
+    {
+        public void show() 
+        {
+            Console.WriteLine("this is public method !");
+        }
+    }
+    class Child : Parent
+    {
+        public new void show() 
+        {
+            //base.show();
+            Console.WriteLine("this is a child method ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Child();
+            p.show();
+            //((Parent)obj).show(); 
+            //obj.show();
+        }   
+    }
+}
