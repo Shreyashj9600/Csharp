efcc1df412b83c5098315362128e6fc2a1649726
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-20 15:13:15 +0530

    Method or function overloading

---
+++ 08-POLYMORPHISM/POLYMORPHISM/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace POLYMORPHISM
+{
+
+    internal class Program
+    {
+        // Method or function overloading
+
+        public void Add()
+        {
+            int a = 11; 
+            int b = 10;
+            int c = a + b;
+            Console.WriteLine("addation result is {0}", c);
+        }
+        public void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+        public void Add(string name , string Lname)
+        {
+            string fullName = name + " " + Lname;
+            Console.WriteLine("your full name is " + fullName);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Add("shreyash" , "jadhav");
+        }
+    }
+}
