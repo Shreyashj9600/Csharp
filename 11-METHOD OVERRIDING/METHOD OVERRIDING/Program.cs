62a3e08ad85e901bc3aba2030b4b1b3bd2330e59
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-23 04:15:59 +0530

    Method overloading

---
+++ 11-METHOD OVERRIDING/METHOD OVERRIDING/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace METHOD_OVERRIDING
+{
+    class Parent
+    { 
+        public virtual void print()
+        {
+            Console.WriteLine("this is a method of PARENT  class");
+        }
+    }
+    class Child : Parent
+    {
+        public override void print()
+        {
+            Console.WriteLine("this is a method of CHILD class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Child();
+            p.print();  
+        }
+    }
+}
