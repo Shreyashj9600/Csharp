ca6d34a3f1e02b079a5423a6f625bbcf786ca92a
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-23 04:18:13 +0530

    sealed class

---
+++ 12-SEALED CLASS/SEALED CLASS/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SEALED_CLASS
+{
+    sealed class Parent 
+    {
+        public void show1()
+        {
+            Console.WriteLine("this is a parent classs");
+        }
+    }
+    //class Child : Parent
+    //{
+    //    public void show2()
+    //    {
+    //        Console.WriteLine("this is a child classs");
+    //    }
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Child c = new Child();
+            //c.show1();
+        }
+    }
+}
