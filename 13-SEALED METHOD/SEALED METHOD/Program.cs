947ac139acdef978d4303419ac7fcc45ab79f287
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-23 04:19:40 +0530

    sealed method

---
+++ 13-SEALED METHOD/SEALED METHOD/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SEALED_METHOD
+{
+    class A
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("This is a method of class A");
+        }
+    }
+    class B : A
+    {
+        public sealed override void print()
+        {
+            Console.WriteLine("This is a method of class B");
+        }
+    }
+
+    //class C : B 
+    //{
+    //    public override void print()
+    //    {
+    //        Console.WriteLine("This is a method of class C");
+    //    }
+    //}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B obj = new B();
+            obj.print();
+        }
+    }
+}
