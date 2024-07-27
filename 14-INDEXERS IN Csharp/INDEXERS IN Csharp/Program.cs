a671c7edd0088a1ef643b1c854ab5115ae1424e0
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-28 05:23:37 +0530

    indexer in c sharp

---
+++ 14-INDEXERS IN Csharp/INDEXERS IN Csharp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace INDEXERS_IN_Csharp
+{
+    class Employee
+    {
+        private int[] myArr = new int[3];
+
+        public int this[int index]
+        {
+            set 
+            {
+                if (index >= 0 && index < myArr.Length)
+                {
+                    if (value > 0)
+                    {
+                        myArr[index] = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("value is not negative ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter correct index ! ");
+                }
+                   
+            }
+            get 
+            {
+                return myArr[index];
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Employee emp = new Employee();
+            emp[0] = 5;
+            Console.WriteLine(emp[0]);
+        }
+    }
+}
