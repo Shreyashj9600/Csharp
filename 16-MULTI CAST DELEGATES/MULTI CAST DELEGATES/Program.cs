cd96b40d13e13942a46e2336159a5b90fcc3ea2c
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-31 00:23:02 +0530

    MULTI CAST DELEGATES

---
+++ 16-MULTI CAST DELEGATES/MULTI CAST DELEGATES/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MULTI_CAST_DELEGATES
+{
+    public delegate void Calculation(int a , int b);
+    internal class Program
+    {
+        public static void Addation(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("Addation result is " + result);
+        }
+        public static void Addation(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine("Subtraction result is " + result);
+        }
+        public static void Multiplaction(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine("Multiplaction result is " + result);
+        }
+        public static void Division(int a, int b)
+        {
+            int result = a / b;
+            Console.WriteLine("Division result is " + result);
+        }
+        static void Main(string[] args)
+        {
+            Program.Addation(1, 2);
+            Program.Subtraction(1, 2);
+
+            Console.ReadLine();
+        }
+    }
+}
