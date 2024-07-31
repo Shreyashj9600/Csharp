30b532a75e693a94e880a29c4a70ac5b3cc32406
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-31 19:02:07 +0530

    ANONYMOUS FUNCTION

---
+++ 17-ANONYMOUS FUNCTION/ANONYMOUS FUNCTION/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ANONYMOUS_FUNCTION
+{
+    public delegate void myDelegate(int num);
+
+    internal class Program
+    {
+        public static void myMethod(myDelegate del, int a)
+        {
+            a += 10;
+            del.Invoke(a);
+        }
+        static void Main(string[] args)
+        {
+            Program.myMethod(delegate (int b)
+            {
+                b += 10;
+                Console.WriteLine(b);
+            }, 5);  
+        }
+    }
+}
